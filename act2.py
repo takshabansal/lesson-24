8fe7df283953af675e7e99181db4c2cc5bfccb7e
takshabansal <takshabansal@gmail.com>
2025-10-18 10:00:58 +0530

    l arrays and sets

---
+++ act2.py
@@ -0,0 +1,11 @@
+setx={"green", "blue"}
+sety={"blue", "yellow"}
+print("Original set elements:")
+print(setx)
+print(sety)
+print("\nIntersection of two said sets:")
+setz=setx.intersection(sety)
+seta=setx.symmetric_difference(sety)
+print(setz)
+print("Symmetric difference of the two sets:")
+print(seta)
